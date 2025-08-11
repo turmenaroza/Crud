416fd4dda3a1c2b0a7d8f2d7bbf772b84108289e
Julia_roza <Julia_roza@estudante.sesisenai.org.br>
2025-08-11 14:34:49 -0300

    add arrumei conexao

--- conexao.php
+++ conexao.php
@@ -2,12 +2,12 @@
 // Conexão com o banco (contém erro de variável e de conexão)
 $host = "localhost";
 $user = "root";
-$password = "";
+$password = "root";
 $db = "crud_exemplo";
 
-$conn = mysqli_connect($hot, $user, $password, $db); // Erro: $hot ao invés de $host
+$conn = mysqli_connect($host, $user, $password, $db); // Erro: $hot ao invés de $host
 
-if (!$conn) {
-    echo "Falha na conexão!";
+if ($conn->connect_error) {
+   die("Conexao falhou: " . $conn->connect_error);
 }
 ?>
