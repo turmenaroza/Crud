cf7ad1d372b231345b24957da0267dbf635182d2
Julia_roza <Julia_roza@estudante.sesisenai.org.br>
2025-08-11 14:21:59 -0300

    add arrumei os erros do cadastrar

--- cadastrar.php
+++ cadastrar.php
@@ -1,23 +1,43 @@
 <?php
-// Cadastro com erros de sintaxe e falta de validação
+
 include("conexao.php");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nome = $_POST["nome"];
-    $email = $_POST["email"]
+    $email = $_POST["email"];
 
     $sql = "INSERT INTO usuarios (nome, email) VALUES ('$nome', '$email')";
     $res = mysqli_query($conn, $sql);
-    if ($res) {
+    if ($$conn->query($sql) === true) {
         echo "Usuário cadastrado com sucesso!";
-    else
-        echo "Erro ao cadastrar!";
+    }else{
+        echo "Erro ao cadastrar!". $sql . '<br>' . $conn->error;
+    }
+        $conn->close();
 }
 
 ?>
 
-<form method="POST">
-    Nome: <input type="text" name="nome"><br>
-    Email: <input type="email" name="email"><br>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastrar</title>
+</head>
+
+<body>
+<form method="POST" action="cadastrar.php"> 
+
+     <label for="name">Nome:</label>
+        <input type="text" name="name" required>
+
+        <label for="email">Email:</label>
+        <input type="email" name="email" required>
+
     <input type="submit" value="Cadastrar">
 </form>
+
+</body>
+
+</html>
